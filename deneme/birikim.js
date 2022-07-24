da6a1eb1bcfe41ffa32a78b09f536df980c0339e
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-07-24 13:56:39 +0300

    v1

--- deneme/birikim.js
+++ deneme/birikim.js
@@ -1,1 +1,12 @@
-if(isNaN(yas)){ divEl.innerHTML= 55; }
+var firstName = 'Ahmet Enes';
+var soyad = 'Dur'
+let email = "kodluyoruz@kodluyoruz.org"
+
+console.log(email.slice(10+1));
+console.log(firstName.slice(1,4));
+
+let domain = email.slice(email.search("@")+1); 
+console.log(domain); //"kodluyoruz.org"
+
+email = email.replace("kodluyoruz.org", "gmail.com");
+console.log(email); //"kodluyoruz@gmail.com"
