4dfe477673eb4287ded9bae2c743948a3455e608
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-07-24 21:59:36 +0300

    v2

--- deneme/birikim.js
+++ deneme/birikim.js
@@ -1,12 +1,33 @@
-var firstName = 'Ahmet Enes';
-var soyad = 'Dur'
-let email = "kodluyoruz@kodluyoruz.org"
+//Add
+const div=document.getElementById("add");
+div.classList.add("fancy","add","addMetod");
 
-console.log(email.slice(10+1));
-console.log(firstName.slice(1,4));
+const classNames = div.classList;
+document.getElementById("pAdd").innerHTML="Class ekledikten sonra liste: "+ classNames;
 
-let domain = email.slice(email.search("@")+1); 
-console.log(domain); //"kodluyoruz.org"
+//Remove
+div.classList.remove("addMetod");
+document.getElementById("pRemove").innerHTML="Class sildikten sonra liste: "+ classNames;
 
-email = email.replace("kodluyoruz.org", "gmail.com");
-console.log(email); //"kodluyoruz@gmail.com"
+//Item
+ const firstClassName=div.classList.item(0);
+ document.getElementById("pItem").innerHTML= "İlk class: "+ firstClassName;
+
+//Contains
+const result=div.classList.contains("add");
+document.getElementById("pContainsTrue").innerHTML="'add' class'ını içeriyor mu? "+" --> "+ result;
+const result2=div.classList.contains("addMetod");
+document.getElementById("pContainsFalse").innerHTML="'addMetod' class'ını içeriyor mu? "+" --> "+ result2;
+
+//Replace
+div.classList.replace("add","newClass");
+document.getElementById("pReplace").innerHTML=classNames;
+
+//Toggle
+const pToggle=document.getElementById("pToggle")
+const button=document.getElementById("toggleButton");
+button.addEventListener("click",function(){
+pToggle.classList.toggle("colorp");
+})
+//Length
+document.getElementById("length").innerHTML="Class names length:"+" "+div.classList.length;
