c349cc8191e8fcf97382fa1d3fbb37e8c4b75352
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-07-28 15:56:29 +0300

    FatArrow

--- deneme/birikim.js
+++ deneme/birikim.js
@@ -1,15 +1,29 @@
-// let func1 = (a, b) => a+b;
+let movie = { 
 
-// let func2 = function (a, b) {
-//     return a+b;
-//   };
+  name: "La la land",
+  
+  thisInArrow:() => { 
+  console.log("Movie name is " + this.name); // 'this' window'u referans gösterir. Bu yüzden name'yi bulamaz.
+  }, 
+  
+  thisInRegular(){ 
+  console.log("Movie name is " + this.name); // 'this' kendisini referans gösterir ve çalışır.
+  } 
+  
+  };
+  movie.thisInArrow(); // output : Movie name is
+  movie.thisInRegular(); // output : Movie name is La la land
 
-  const carpim = (sayi1, sayi2) => sayi1 * sayi2;
-/*
-    // Daha uzun hali ise;
-    const carpim = function(sayi1,sayi2){
-      return sayi1 * sayi2;
-    }
-  */
+  const newArray = (nums) => {
 
-console.log(carpim(3, 5));
+    const newNums = nums.map(e=>{      
+        if(e%2==0){
+            return e*2
+        }else if(e%2==1){
+            return e*3
+        }
+    });
+   return newNums 
+}
+
+console.log(newArray([1,2,3,4,5]));  // output: [3,4,9,8,15]
