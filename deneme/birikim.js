6fc195cda70af19855a5957884edb7f0fb81eafe
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-07-30 22:09:52 +0300

    formlarlaPratik

--- deneme/birikim.js
+++ deneme/birikim.js
@@ -1,9 +1,45 @@
-// Form Submit:
+let userFormDOM = document.querySelector("#userForm");
+userFormDOM.addEventListener("submit", formHandler);
+const alertDOM = document.querySelector("#alert")
 
-let formDOM = document.querySelector("#userForm")
-formDOM.addEventListener('submit', formSubmit)
+const alertFunction = (title, message, className="warning") => `
+<div class="alert alert-${className} alert-dismissible fade show" role="alert">
+  <strong> ${title} </strong> ${message}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+`
 
-function formSubmit(event) {
-  event.preventDefault() // default işemi engelledik
-  console.log("İşlem Gerçekleşti");
+function formHandler(event) {
+  event.preventDefault(); // default işemi engelledik
+  const USER_NAME = document.querySelector("#username")
+  const SCORE = document.querySelector("#score")
+
+  if (USER_NAME.value && SCORE.value) {
+    addItem(USER_NAME.value, SCORE.value)
+    USER_NAME.value = ""
+    SCORE.value = ""
+  } else {
+    alertDOM.innerHTML = alertFunction(
+      "Başlık Bilgisi!",
+      "Eksik Bilgi Girdiniz.",
+      "danger"
+      )
+    }
+
 }
+
+let userListDOM = document.querySelector("#userList");
+
+function addItem(userName, score) {
+  let liDOM = document.createElement('li')
+  liDOM.innerHTML = `
+  ${userName}
+  <span class="badge bg-primary rounded-pill">${score}</span>
+  `
+  liDOM.classList.add(
+    'list-group-item','d-flex','justify-content-between','align-items-center'
+  )
+
+  userListDOM.append(liDOM)
+}
+
