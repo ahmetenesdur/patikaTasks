f5cd448725f38deaaf2dbab681385a66faf9e6c6
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-07-30 17:07:09 +0300

    preventDefault

--- deneme/birikim.js
+++ deneme/birikim.js
@@ -1,14 +1,9 @@
-var username;
-var password;
-function handleSubmit(e) {
-  e.preventDefault();
-  console.log("username:" + username + "-" + "password:" + password);
-}
-function handleChange(e) {
-  if (e.target.name === "username") {
-    username = e.target.value;
-  }
-  if (e.target.name === "password") {
-    password = e.target.value;
-  }
+// Form Submit:
+
+let formDOM = document.querySelector("#userForm")
+formDOM.addEventListener('submit', formSubmit)
+
+function formSubmit(event) {
+  event.preventDefault() // default işemi engelledik
+  console.log("İşlem Gerçekleşti");
 }
