162a008913a76aa6e0f383121714f0c99a450569
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-08-01 22:41:48 +0300

    forEach

--- ekDeneme/deneme.js
+++ ekDeneme/deneme.js
@@ -1,13 +1,13 @@
-function vowelsAndConsonants(s) {
-    let vowel = ["a", "e", "i", "o", "u"]
-    for(let word in s){
-        if(vowel.includes(s[word])){
-            console.log(s[word]);
-        }
-    }
-    for(let word in s){
-        if(!vowel.includes(s[word])){
-            console.log(s[word]);
-        }
-    }
-}
+const numbers = [4, 11, 9];
+const newArray = [];
+
+numbers.forEach(function(numbers){
+  newArray.push(numbers*3);
+});
+console.log(newArray);
+
+const sayi = [12, 24, 36];
+let arr = [];
+
+sayi.forEach(sayi => arr.push(sayi+2))
+console.log(arr);
