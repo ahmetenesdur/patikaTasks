d82098cae701c4e9b31c985be3e19636b5f39d53
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-08-01 16:17:39 +0300

    Day2:Loops

--- ekDeneme/deneme.js
+++ ekDeneme/deneme.js
@@ -1,12 +1,13 @@
-let nums = [2, 3, 6, 6, 5]
-
-function getSecondLargest(nums) {
-    let newNums = nums.slice()
-    newNums.sort(function(a,b){return b-a})
-
-    return newNums.find(function (element){
-        return newNums[0] > element
-    })
+function vowelsAndConsonants(s) {
+    let vowel = ["a", "e", "i", "o", "u"]
+    for(let word in s){
+        if(vowel.includes(s[word])){
+            console.log(s[word]);
+        }
+    }
+    for(let word in s){
+        if(!vowel.includes(s[word])){
+            console.log(s[word]);
+        }
+    }
 }
-
-console.log(getSecondLargest(nums));
