352aec61db15d9d42bfa6ef593d838fb3fdf421f
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-08-02 21:24:08 +0300

    toDoListstart

--- ekDeneme/deneme.js
+++ ekDeneme/deneme.js
@@ -1,13 +1,10 @@
-const numbers = [4, 11, 9];
-const newArray = [];
-
-numbers.forEach(function(numbers){
-  newArray.push(numbers*3);
+const maaslar = [ 1100, 13000, 2500, 4500, 1500, 25000, 2000 ];
+const yeniMaaslar = maaslar.map((e)=>{
+    if(e > 3000)
+        return e * 1.15;
+    else
+        return e * 1.25;
 });
-console.log(newArray);
 
-const sayi = [12, 24, 36];
-let arr = [];
-
-sayi.forEach(sayi => arr.push(sayi+2))
-console.log(arr);
+console.log( yeniMaaslar );
+//[1210, 13650, 2750, 4725, 1650.0000000000002, 26250, 2200]
