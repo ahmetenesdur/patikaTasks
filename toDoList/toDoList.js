9f9175dd47b5eb5bf5d08633343c0011c306999f
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-08-03 20:30:28 +0300

    toDoListFinsihed

--- toDoList/toDoList.js
+++ toDoList/toDoList.js
@@ -1,22 +1,19 @@
-let listDOM = document.querySelector('#list')   //Eklenecek liste seçimi
-let LocalTask = {id:"", task:"" , check: false} //LocalStorage bilgilerini tutacak object
-let ArrayTask = []                              //LocalStorage için array
-let i=0;                                        //id ataması için değişken.(bu sayede liste elemanlarına daha kolay erişebileceğiz)
+let listDOM = document.querySelector('#list')
+let LocalTask = { id: "", task: "", check: false }
+let ArrayTask = []
+let i = 0;
 
-//LocalStorage işlemleri
-if(localStorage.getItem('todo'))    //LocalStorage varsa gir
-{   
-    ArrayTask = JSON.parse(localStorage.getItem('todo')) //LocalStorage çağır
-    ArrayTask.forEach(function (element)                 //LocalStorage 'deki bilgiyi aktarmak için fonksiyon
-    {
+if (localStorage.getItem('todo')) {
+    ArrayTask = JSON.parse(localStorage.getItem('todo'))
+    ArrayTask.forEach(function (element) {
         i++;
-        element.id = `id${i}`;                           //LocalStorage yeni id ataması
+        element.id = `id${i}`;
         localStorage.setItem('todo', JSON.stringify(ArrayTask))
 
-        let liDOM = document.createElement(`li`)        //liste elemanını oluşturma
-        liDOM.setAttribute('id',`id${i}`)               //liste elemanına "id" atama
-        liDOM.innerHTML =                               //liste elemanına Eklencek bilgiyi atama 
-        `
+        let liDOM = document.createElement(`li`)
+        liDOM.setAttribute('id', `id${i}`)
+        liDOM.innerHTML =
+            `
         ${element.task} 
         <button
         class="close" 
@@ -25,60 +22,48 @@ if(localStorage.getItem('todo'))    //LocalStorage varsa gir
         >x
         </button>
         `
-/*      (${element.task} liste elemanı bilgisi 
-        </button>        silme butonu "x" 
-        RemoveFunc(${i}) silme fonksiyonu       */
 
-        listDOM.append(liDOM)    //liste elemanını listenin sonuna ekleme
-        if(ArrayTask[i-1].check) //liste elemanı "check" edilmiş ise işaretleme
-        {
-            let changeLi = document.querySelector(`#id${i}`) //liste elamanının ataması
-            changeLi.classList.add("checked")                //liste elamanının "checked" classını ekle
+        listDOM.append(liDOM)
+        if (ArrayTask[i - 1].check) {
+            let changeLi = document.querySelector(`#id${i}`)
+            changeLi.classList.add("checked")
         }
     });
 }
 
-
-// Ekle butonunu <span> tipinden <button> tipine çevirme. (bu şekilde listeye "enter" ile ekleme yapılabilcek)
 let elem = document.querySelector('#liveToastBtn')
 elem.outerHTML = `<button type="submit" onclick="newElement()" id="liveToastBtn" class="button" style ="border-width: 0px">${elem.innerHTML}</button>`;
 
-
-//Kutuya yazılan bilgiyi alma
 let userTaskDOM = document.querySelector('#userTask')
 userTaskDOM.addEventListener('submit', formHandler)
 
-//Kutudaki bilgi (input) için işlemler
 function formHandler(event) {
-    event.preventDefault()                          //Sayfa yenilenmesini engeleme
-    const TASK = document.querySelector("#task")    //Kutudaki bilginin atanması
-    
-    if (TASK.value.trim() == ""){   //"input" değeri boş ise "toast" bildirimi ile 
-        $(".error").toast("show");  //"Listeye boş ekleme yapamazsınız!" bildirimini göster
-    } 
-    else{
-        addItem(TASK.value)          //Bilgi ekleme fonksiyonunu çalıştır
-        TASK.value = ""              //Gonderdikten sonra "input" sıfırlama 
-        $(".success").toast("show"); //"Bilgi eklendi" bildirimini göster
+    event.preventDefault()
+    const TASK = document.querySelector("#task")
+
+    if (TASK.value.trim() == "") {
+        $(".error").toast("show");
+    }
+    else {
+        addItem(TASK.value)
+        TASK.value = ""
+        $(".success").toast("show");
     }
 }
 
-
-//Bilgi ekleme fonksiyonu
-const addItem = (task) => 
-{ 
+const addItem = (task) => {
     i++;
 
-    LocalTask.task = task;    //"object" bilgiyi gönder
-    LocalTask.id = `id${i}`;  //"object" id gönder
-    ArrayTask.push(LocalTask) //LocalStorage için "object" teki bilgileri gönder
+    LocalTask.task = task;
+    LocalTask.id = `id${i}`;
+    ArrayTask.push(LocalTask)
     localStorage.setItem('todo', JSON.stringify(ArrayTask))
-    ArrayTask = JSON.parse( localStorage.getItem('todo'))
+    ArrayTask = JSON.parse(localStorage.getItem('todo'))
 
-    let liDOM = document.createElement(`li`)        //liste elemanını oluşturma
-    liDOM.setAttribute('id',`id${i}`)               //liste elemanına "id" atama
-    liDOM.innerHTML =                               //liste elemanına Eklencek bilgiyi atama 
-    `
+    let liDOM = document.createElement(`li`)
+    liDOM.setAttribute('id', `id${i}`)
+    liDOM.innerHTML =
+        `
     ${task} 
     <button 
     class="close" 
@@ -87,36 +72,32 @@ const addItem = (task) =>
     >x
     </button>
     `
-    listDOM.append(liDOM)       //liste elemanını listenin sonuna ekleme
+    listDOM.append(liDOM)
 }
 
-//Silme fonksiyonu
-function RemoveFunc(j) {                                        //"j" id numarası
-    const element = document.querySelector(`#id${j}`);          //silinecek liste elemanını için atama
+function RemoveFunc(j) {
+    const element = document.querySelector(`#id${j}`);
 
-    let index = ArrayTask.findIndex(function (Atask) {          //silinecek liste elemnını LocalStorage içindeki "index"ine ulaşmak
+    let index = ArrayTask.findIndex(function (Atask) {
         return JSON.stringify(Atask).indexOf(`id${j}`) >= 0
     });
-        ArrayTask.splice(index, 1)                              //silinecek liste elemanını LocalStorage içinden silme
-        localStorage.setItem('todo', JSON.stringify(ArrayTask)) //sildikten sonra tekrar set etmemiz lazım
-        ArrayTask = JSON.parse( localStorage.getItem('todo') )
-        element.remove();                                       //liste elemanını silme
+    ArrayTask.splice(index, 1)
+    localStorage.setItem('todo', JSON.stringify(ArrayTask))
+    ArrayTask = JSON.parse(localStorage.getItem('todo'))
+    element.remove();
 }
 
-//Checked işlemi
-document.addEventListener('click', (element) =>                 //tıklanan liste elemanına ulaşma
-{
-    if(element.target.matches('li'))                            //liste elemanı mı diye kontrol
-    {
-        let elementId = element.target.id;                      //liste elemanının "id" sine ulaşma
-        let index = ArrayTask.findIndex(function (Atask) {      //liste elemanının LocalStorage içindeki "index"ine ulaşmak
+document.addEventListener('click', (element) => {
+    if (element.target.matches('li')) {
+        let elementId = element.target.id;
+        let index = ArrayTask.findIndex(function (Atask) {
             return JSON.stringify(Atask).indexOf(`${elementId}`) >= 0
         });
-        ArrayTask[index].check = !(ArrayTask[index].check)      //LocalStorage içindeki "check" bilgisini değiştirme
-        localStorage.setItem('todo', JSON.stringify(ArrayTask)) //değiştirdikten sonra tekrar set etmemiz lazım
-        ArrayTask = JSON.parse( localStorage.getItem('todo') )
-        
-        let changeLi = document.querySelector(`#${elementId}`) //liste elamanının ataması
-        changeLi.classList.toggle("checked")                   //liste elamanına "checked" class bilgisi varsa kaldır yoksa ekle
+        ArrayTask[index].check = !(ArrayTask[index].check)
+        localStorage.setItem('todo', JSON.stringify(ArrayTask))
+        ArrayTask = JSON.parse(localStorage.getItem('todo'))
+
+        let changeLi = document.querySelector(`#${elementId}`)
+        changeLi.classList.toggle("checked")
     }
 });
