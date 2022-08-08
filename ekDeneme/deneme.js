83dd07d59960a9403a3a6c67ef019cffd38ffc37
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-08-08 20:02:48 +0300

    asiankitchen

--- ekDeneme/deneme.js
+++ ekDeneme/deneme.js
@@ -1,12 +1,20 @@
-function Insan(isim,yas) {
-  this.isim = isim;
-  this.yas = yas;
-}
+const toplama = ({ sayi1, sayi2, ...args }) => {
+  let sonuc = sayi1 + sayi2;
+  for (let sayi in args){
+   sonuc += args[sayi]
+   console.log(sayi);
+  }
+  return sonuc;
+ }
+ const sayilar = {
+  sayi1: 1,
+  sayi2: 2,
+  sayi3: 3,
+  sayi4: 4,
+  sayi5: 5
+ }
 
-const ayse = new Insan("ayşe",22);
+ 
+  console.log(toplama(sayilar));
 
-
-ayse.__proto__.enYeniFonksiyon = () => {console.log("Tekrar Merhaba Kodluyoruz!")}
-ayse.enYeniFonksiyon();
-// Output : "Tekrar Merhaba Kodluyoruz!"
-console.log(ayse);
+  document.getElementById("demo").innerHTML = toplama(sayilar); 
