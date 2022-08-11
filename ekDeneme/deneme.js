d266d399cc3c3b8b9e6edf35279f4e2e59595795
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-08-11 20:25:30 +0300

    toTitleCase

--- ekDeneme/deneme.js
+++ ekDeneme/deneme.js
@@ -1,18 +1,10 @@
-function reverseString(str) {
-  // Step 1. Use the split() method to return a new array
-  var splitString = str.split(""); // var splitString = "hello".split("");
-  // ["h", "e", "l", "l", "o"]
+const toTitleCase = (phrase) => {
+  return phrase
+    .toLowerCase()
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+};
 
-  // Step 2. Use the reverse() method to reverse the new created array
-  var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
-  // ["o", "l", "l", "e", "h"]
-
-  // Step 3. Use the join() method to join all elements of the array into a string
-  var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
-  // "olleh"
-  
-  //Step 4. Return the reversed string
-  return joinArray; // "olleh"
-}
-
-reverseString("hello");
+let result = toTitleCase('maRy hAd a lIttLe LaMb');
+console.log(result);
